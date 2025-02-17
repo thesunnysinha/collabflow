056dcb6a4a2264db252b27d8d3a6acebd4a9c101
Sunny Kumar Sinha <thesunnysinha@gmail.com>
2025-02-17 13:56:24 +0530

    Update deployment workflow to use SSH identity file for authentication

--- scripts/deploy_to_vm.py
+++ scripts/deploy_to_vm.py
@@ -10,8 +10,8 @@ runner = SetupRunner(config)
 ssh_configs = [
     SSHConfig(
         ssh_username=os.environ.get('GITHUB_SSH_USERNAME'),
-        ssh_password=os.environ.get('GITHUB_SSH_PASSWORD'),
         ssh_hostname=os.environ.get('GITHUB_SSH_HOSTNAME'),
+        ssh_identity_file=os.environ.get('GITHUB_SSH_IDENTITY_FILE'),
     ),
 ]
 
